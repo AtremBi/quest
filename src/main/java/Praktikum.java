2af5d22e498bf1d94bcd4e6eb0490e8a4ccc231b
ArtemBi <avatarus2010@yandex.ru>
2022-10-03 13:28:57 +0300

    fix

--- src/main/java/Praktikum.java
+++ src/main/java/Praktikum.java
@@ -18,9 +18,6 @@ public class Praktikum {
         if (year % 4 == 0 || year % 400 == 0){
             return true;
         }
-        else if(year % 100 != 0) {
-            return false;
-        }
         return false;
     }
 }
