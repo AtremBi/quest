9127b51ae680f599af7c0a0ba31d39f166e3134e
ArtemBi <avatarus2010@yandex.ru>
2022-10-03 13:42:57 +0300

    fix

--- src/main/java/Praktikum.java
+++ src/main/java/Praktikum.java
@@ -15,7 +15,7 @@ public class Praktikum {
     }
 
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0 || year % 400 == 0){
+        if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0){
             return true;
         }
         return false;
